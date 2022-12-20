b2f749883883f6371609a87ede8193fee1bc581e
JamesKwong <111725853+goodqthe123@users.noreply.github.com>
2022-12-20 19:54:21 +0800

    Add files via upload

---
+++ precache-manifest.909d6ec48bb1b887df5a22b7a495561f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5daff82ceffb168f4c1df143681f92",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23f01a1ac1eeaec29a3d",
+    "url": "./static/css/main.ee5d6995.chunk.css"
+  },
+  {
+    "revision": "e8139adf6ae224d26af8",
+    "url": "./static/js/2.2d56980a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2d56980a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f01a1ac1eeaec29a3d",
+    "url": "./static/js/main.6b061255.chunk.js"
+  },
+  {
+    "revision": "42a33bc61b96354c527f",
+    "url": "./static/js/runtime-main.71a52f47.js"
+  }
+]);
